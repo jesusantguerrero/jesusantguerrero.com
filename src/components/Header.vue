d56aaddb31d21676f8fe58b6e4763315ddb25e5b
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-10-05 02:49:06 -0400

    feat: add copy

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,18 @@
 <template>
-<div class="fixed z-10 w-full h-16 transition" :class="{'shadow-md bg-primary-500': scrolled}">
-    <div class="wrapper" v-if="scrolled"></div>
+<div class="fixed z-10 w-full px-10 py-3 text-gray-400 transition" :class="{'shadow-md bg-accent-500 text-white': scrolled}">
+    <div class="flex justify-between wrapper">
+        <h1 class="text-2xl font-bold">JG</h1>
+        <nav class="flex space-x-2 font-medium list-none">
+            <li class="cursor-pointer">Home</li>
+            <li class="cursor-pointer">Projects</li>
+            <li class="cursor-pointer">Blog</li>
+            <li class="cursor-pointer">More</li>
+        </nav>
+    </div>
 </div>
 </template>
 
-<script >
-
+<script>
 import { onMounted, ref } from 'vue'
 
 export default {
