6d9f98f13bf1c3f35c3d6b9403aee33a758c8aa3
Jesus Guerrero <jesusant.guerrero@gmail.com>
2024-09-16 16:10:20 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
+import { loadEnv } from "vite";
 
 import netlify from "@astrojs/netlify";
 import node from "@astrojs/node";
